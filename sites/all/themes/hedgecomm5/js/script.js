c51466fc9d375b85c1fec4a267493a947ad5060b
Gerrit Vos <gerrit@hedgecomm.be>
2013-02-02 21:13:48 +0100

    Deelnemers view

--- sites/all/themes/hedgecomm5/js/script.js
+++ sites/all/themes/hedgecomm5/js/script.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function($) {
 	$('select').uniform();
 	
 	// border-bottom-color animatie gewone links
-	$('#columns a:not(.btn)').hover(function() {
+	$('#columns a:not(.btn,.icon)').hover(function() {
 		$(this).stop().animate({borderBottomColor: '#000000', color: '#000000'}, 250);
 	}, function() {
 		$(this).stop().animate({borderBottomColor: '#ceb2B2', color: '#360808'}, 250);
